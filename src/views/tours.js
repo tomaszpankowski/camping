715646801edff21bd35759f8fafb74f096bdfd2b
tomaszpankowski <pankowski.email@gmail.com>
2022-02-07 16:32:05 +0100

    layout done

--- src/views/tours.js
+++ src/views/tours.js
@@ -1,11 +1,18 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import ToursS1 from "../components/tours-s1";
+import ToursS2 from "../components/tours-s2";
+import ToursS3 from "../components/tours-s3";
+import ToursS4 from "../components/tours-s4";
 
 class Tours extends Component{
     render(){
         return(        
-            <Container fluid className="tours-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <ToursS1/>
+                <ToursS2/>
+                <ToursS3/>
+                <ToursS4/>
             </Container>    
         );
     }
